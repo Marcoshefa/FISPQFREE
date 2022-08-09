59573d138405aeb26b46a2f30e61f316e3165d77
marcosfn <marcoshefa@gmail.com>
2022-08-09 19:55:43 -0300

    e

--- api/app.py
+++ api/app.py
@@ -8,8 +8,8 @@ CORS(app)
 
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 app.config['MYSQL_DATABASE_PORT'] = 3306
-app.config['MYSQL_DATABASE_USER'] = 'admin01'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Faby162283!'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Marcos21111984!'
 app.config['MYSQL_DATABASE_DB'] = 'DB_FISPQ'
 
 mysql.init_app(app)
