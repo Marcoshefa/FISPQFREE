273f327e6d07f7cb57677fb3ffc681ad214d7a22
marcosfn <marcoshefa@gmail.com>
2024-04-15 21:36:11 -0300

    4

--- api/modules/fispq/routes.py
+++ api/modules/fispq/routes.py
@@ -178,6 +178,6 @@ def gerar_pdf(id_fispq):
     
     pdf_id = gerar_pdf_fispq(id_fispq)
 
-    resultado = f"../pdfs/{pdf_id}.pdf"
-    # resultado = f"./pdfs/{pdf_id}.pdf"
+    # resultado = f"../pdfs/{pdf_id}.pdf"
+    resultado = f"./pdfs/{pdf_id}.pdf"
     return send_file(resultado)
